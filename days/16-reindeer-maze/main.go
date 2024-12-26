f29f7b188c75501d0fb07b6e5733c5950dffe29a
veno501 <veno.banovsek@gmail.com>
2024-12-26 02:51:29 +0100

    day 16 challenge 2 solution

--- days/16-reindeer-maze/main.go
+++ days/16-reindeer-maze/main.go
@@ -6,7 +6,6 @@ import (
 	"math"
 	"os"
 	"path/filepath"
-	"slices"
 )
 
 type Point struct {
@@ -20,17 +19,7 @@ type State struct {
 	Point     Point
 	Direction Direction
 	Cost      int
-	Estimate  int
 	Parent    *State
-}
-
-type Visitations struct {
-	Cost    int
-	Parents []State
-}
-
-func heuristic(a, b Point) int {
-	return int(math.Abs(float64(a.x-b.x)) + math.Abs(float64(a.y-b.y)))
 }
 
 func tracePaths(endStates [](*State), points map[Point]bool) map[Point]bool {
@@ -41,14 +30,6 @@ func tracePaths(endStates [](*State), points map[Point]bool) map[Point]bool {
 		}
 		tracePaths([]*State{(*endState).Parent}, points)
 	}
-	// for _, parent := range visitations[endState.Point][endState.Direction].Parents {
-	// 	// parent, ok := parents[p]
-	// 	// if !ok {
-	// 	// 	return points
-	// 	// }
-	// 	points[parent.Point] = true
-	// 	points = tracePaths(parent, visitations, parents, points)
-	// }
 	return points
 }
 
@@ -57,71 +38,34 @@ func AStar(maze [][]byte, start, end Point) (int, map[Point]bool) {
 		Point:     start,
 		Direction: Point{1, 0},
 		Cost:      0,
-		Estimate:  heuristic(start, end),
 		Parent:    nil,
 	}}
-	// visitations := make(map[Point]map[Point]Visitations)
-	// parent := make(map[State]State)
-	// nodeCost := make(map[State]int)
 	visited := make(map[Point]map[Direction]int)
-
 	var minCost = math.MaxInt
-	// var endState State
 	var pathEnds = [](*State){}
 
 	for len(queue) > 0 {
-		slices.SortFunc(queue, func(a, b State) int {
-			return (a.Cost + a.Estimate) - (b.Cost + b.Estimate)
-		})
-
 		currentState := queue[0]
 		queue = queue[1:]
-
-		// if _, found := visitations[currentState.Point]; !found {
-		// 	visitations[currentState.Point] = make(map[Point]Visitations)
-		// }
-
-		// if v, visited := visitations[currentState.Point][currentState.Direction]; !visited || currentState.Cost < v.Cost {
-		// 	visitations[currentState.Point][currentState.Direction] = Visitations{currentState.Cost, []State{}}
-		// }
-		// v, _ := visitations[currentState.Point][currentState.Direction]
-		// if currentState.Cost > v.Cost {
-		// 	continue
-		// } else if currentState.Cost == v.Cost {
-		// 	visitations[currentState.Point][currentState.Direction] = Visitations{currentState.Cost, append(slices.Clone(v.Parents), currentState)}
-		// }
 
 		if _, ok := visited[currentState.Point]; !ok {
 			visited[currentState.Point] = make(map[Direction]int)
 		}
-		if _, ok := visited[currentState.Point][currentState.Direction]; ok {
-			// if cost, ok := visited[currentState.Point][currentState.Direction]; ok && cost < currentState.Cost {
+		if cost, ok := visited[currentState.Point][currentState.Direction]; ok && cost < currentState.Cost {
 			continue
 		} else {
 			visited[currentState.Point][currentState.Direction] = currentState.Cost
 		}
 
 		if currentState.Point == end {
-			if currentState.Cost <= minCost {
+			if currentState.Cost == minCost {
 				minCost = currentState.Cost
-				// endState = currentState
 				pathEnds = append(pathEnds, &currentState)
-
-				// var p State = currentState
-				// for p.Parent != nil {
-				// 	p = *p.Parent
-				// 	visited[p.Point][p.Direction] = math.MaxInt
-				// 	// pathEnds = append(pathEnds, &p)
-				// }
+			} else if currentState.Cost < minCost {
+				minCost = currentState.Cost
+				pathEnds = [](*State){&currentState}
 			}
 			continue
-			// if minCost == -1 || currentState.Cost < minCost {
-			// 	minCost = currentState.Cost
-			// 	bestPaths = [][]Point{currentState.Path}
-			// } else if currentState.Cost == minCost {
-			// 	bestPaths = append(bestPaths, slices.Clone(currentState.Path))
-			// }
-			// continue
 		}
 
 		for _, dir := range []Point{{1, 0}, {0, -1}, {-1, 0}, {0, 1}} {
@@ -132,26 +76,19 @@ func AStar(maze [][]byte, start, end Point) (int, map[Point]bool) {
 						Point:     newPoint,
 						Direction: currentState.Direction,
 						Cost:      currentState.Cost + 1,
-						Estimate:  heuristic(newPoint, end),
-						// Path:      append(slices.Clone(currentState.Path), newPoint),
-						Parent: &currentState,
+						Parent:    &currentState,
 					})
-					// parent[queue[len(queue)-1]] = currentState
 				}
 			} else if !(dir.x == -currentState.Direction.x && dir.y == -currentState.Direction.y) {
 				queue = append(queue, State{
 					Point:     currentState.Point,
 					Direction: dir,
 					Cost:      currentState.Cost + 1000,
-					Estimate:  currentState.Estimate,
-					// Path:      append(slices.Clone(currentState.Path), currentState.Point),
-					Parent: &currentState,
+					Parent:    &currentState,
 				})
-				// parent[queue[len(queue)-1]] = currentState
 			}
 		}
 	}
-
 	trace := tracePaths(pathEnds, make(map[Point]bool))
 
 	return minCost, trace
@@ -175,24 +112,15 @@ func main() {
 		}
 	}
 
-	minScore, trace := AStar(input, startPos, endPos)
-	// for p := range trace {
-	// 	if p == startPos || p == endPos {
-	// 		continue
-	// 	}
-	// 	AStarSameCost(input, p, endPos)
-	// }
+	minScore, trace := AStar(input, endPos, startPos)
+
 	var bestPathTiles int
 	for y := 0; y < len(input); y++ {
 		for x := 0; x < len(input[y]); x++ {
 			if found, ok := trace[Point{x, y}]; found && ok {
 				bestPathTiles++
-				fmt.Print("O")
-			} else {
-				fmt.Print(string(input[y][x]))
 			}
 		}
-		fmt.Println()
 	}
 	fmt.Printf("Lowest score: %d\n", minScore)
 	fmt.Printf("Number of tiles on best paths: %d\n", bestPathTiles)
